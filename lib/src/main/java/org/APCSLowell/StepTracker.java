1bf5e7477e24f8a5e6a6cc7e4b79a8c09c71846a
AlexYuen31415 <alexyuen31415@gmail.com>
2026-04-26 11:06:13 +0000

    commit

--- lib/src/main/java/org/APCSLowell/StepTracker.java
+++ lib/src/main/java/org/APCSLowell/StepTracker.java
@@ -1,8 +1,27 @@
 package org.APCSLowell;
 
 public class StepTracker {
-    public StepTracker(int activityThreshold) {}
-    void addDailySteps(int stepCount) {}
-    int activeDays() {}
-    double averageSteps() {}
+    private int minSteps;
+    private int totalSteps = 0;
+    private int totalDays = 0;
+    private int totalActiveDays = 0;
+    public StepTracker(int activityThreshold) {
+        minSteps = activityThreshold;
+    }
+    public void addDailySteps(int stepCount) {
+        totalSteps += stepCount;
+        totalDays++;
+        if (stepCount >= minSteps) {
+            totalActiveDays++;
+        }
+    }
+    public int activeDays() {
+        return totalActiveDays;
+    }
+    public double averageSteps() {
+        if (totalDays == 0) {
+            return 0;
+        }
+        return (double)totalSteps/totalDays;
+    }
 }
